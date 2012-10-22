d5158a5df02225f3ac31d2e0a46b7d58a51d7419
Matt Buland <mbuland@mymail.mines.edu>
2012-10-22 11:20:24 -0600

    Successfully acquire gl Context, and change the background to green (arbitrarily)

--- rubiks.js
+++ rubiks.js
@@ -1,30 +1,30 @@
+gl=null;
 function glSetup() {
 
-	var canvas = getElementById("drawingarea");
+	var canvas = document.getElementById("glArea");
+	console.log("Initing gl for canvas: " + canvas);
+	//Initialize the global variable gl to null.
+	gl=null;
+	try{
+		//Try to grab the standard context. If it fails,fallback to experimental.
+		gl=canvas.getContext("webgl")||canvas.getContext("experimental-webgl");
+	} catch(e) {}
 
-	initWebGl(canvas);
+	//If we don't have a GLcontext, give up now
+	if(!gl) {
+		alert("UnabletoinitializeWebGL.Yourbrowsermaynotsupportit.");
+		return;
+	}
+	console.log("Done initing");
 
 	if (gl) {
 		gl.clearColor(0.0, 69.0, 0.0, 1.0);
 		gl.enable(gl.DEPTH_TEST);
 		gl.depthFunc(gl.LEQUAL);
-		gl.clear(gl.COLOR_BUFFER_BIT|g.DEPTH_BUFFER_BIT);
+		gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
 	}
 }
-
-function initWebGL(canvas) {
-	//Initializetheglobalvariablegltonull.
-	gl=null;
-	try{
-		//Trytograbthestandardcontext.Ifitfails,fallbacktoexperimental.
-		gl=canvas.getContext("webgl")||canvas.getContext("experimental-webgl");
-	}
-	catch(e){}
-
-	//If we don't have a GLcontext, give up now
-	if(!gl) {
-		alert("UnabletoinitializeWebGL.Yourbrowsermaynotsupportit.");
-	}
+function loaded() {
+	glSetup();
 }
-
-glSetup();
+window.onload = function(){loaded()};
