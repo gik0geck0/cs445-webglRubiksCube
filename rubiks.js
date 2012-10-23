96bf67fcca9de78d1aeed550474e08e185b23c00
John Kelly <johkelly@mymail.mines.edu>
2012-10-22 20:52:00 -0600

    Huzzah I can draw a triangle. Can we get 3D soon?

--- rubiks.js
+++ rubiks.js
@@ -1,32 +1,59 @@
-var gl;
-
 function glSetup() {
 
-	var canvas = getElementById("drawingarea");
+        var canvas = document.getElementById("drawingArea");
 
-	initWebGl(canvas);
+        var gl = initWebGL(canvas);
 
-	if (gl) {
-		gl.clearColor(0.0, 69.0, 0.0, 1.0);
-		gl.enable(gl.DEPTH_TEST);
-		gl.depthFunc(gl.LEQUAL);
-		gl.clear(gl.COLOR_BUFFER_BIT|g.DEPTH_BUFFER_BIT);
-	}
+        if (gl) {
+                gl.clearColor(0, 0, 69.0, 1.0);
+                gl.enable(gl.DEPTH_TEST);
+                gl.depthFunc(gl.LEQUAL);
+                gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+        }
+        
+        //var v_source = document.getElementById("vs").textContent;
+        //var f_source = document.getElementById("fs").textContent;
+	var v_source = 'attribute vec2 pos;' +
+		 'void main() { gl_Position = vec4(pos, 0, 1); }';
+	var f_source = 'precision mediump float;' +
+		'void main() { gl_FragColor = vec4(0,0.8,0,1); }';
+        
+        var v_shader = compileShader(gl, v_source, gl.VERTEX_SHADER);
+        var f_shader = compileShader(gl, f_source, gl.FRAGMENT_SHADER);
+        
+        var program = createProgram(gl, v_shader, f_shader);
+        
+        gl.useProgram(program);
+	
+        program.vertexPosAttrib = gl.getAttribLocation(program, 'pos');
+        
+        var buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+	
+	//var modelArray = make2DVertexPositionArray();
+	var modelArray = [0, 0, .1, 0, 0, .1];
+	
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(modelArray), gl.STATIC_DRAW);
+	
+	gl.enableVertexAttribArray(program.vertexPosAttrib);
+	gl.vertexAttribPointer(program.vertexPosAttrib, 2, gl.FLOAT, false, 0, 0);
+	gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 
 function initWebGL(canvas) {
-	//Initializetheglobalvariablegltonull.
-	gl=null;
-	try{
-		//Trytograbthestandardcontext.Ifitfails,fallbacktoexperimental.
-		gl=canvas.getContext("webgl")||canvas.getContext("experimental-webgl");
-	}
-	catch(e){}
+        //Initializetheglobalvariablegltonull.
+        var gl=null;
+        try{
+                //Trytograbthestandardcontext.Ifitfails,fallbacktoexperimental.
+                gl=canvas.getContext("webgl")||canvas.getContext("experimental-webgl");
+        }
+        catch(e){}
 
-	//If we don't have a GLcontext, give up now
-	if(!gl) {
-		alert("UnabletoinitializeWebGL.Yourbrowsermaynotsupportit.");
-	}
+        //If we don't have a GLcontext, give up now
+        if(!gl) {
+                alert("UnabletoinitializeWebGL.Yourbrowsermaynotsupportit.");
+        }
+	return gl;
 }
 
 /**
@@ -87,6 +114,30 @@ function createProgram(gl, vertexShader, fragmentShader) {
   return program;
 };
 
+function make3DVertexPositionArray(){
+        var array = [];
+        var m = new Float32Array([ [0, 0, 0]
+                                   [1, 0, 0]
+                                   [0, 1, 0]
+                                   [0, 0, 1]
+                                   [1, 1, 0]
+                                   [1, 0, 1]
+                                   [0, 1, 1]
+                                   [1, 1, 1]] );
+        array.concat(m[0], m[1], m[2]);
+	return array;
+}
+
+function make2DVertexPositionArray(){
+        var array = [];
+        var m = new Float32Array([ [0, 0],
+                                   [1, 0],
+                                   [0, 1],
+                                   [1, 1]] );
+        array.concat(m[0], m[1], m[2]);
+	return array;
+}
+
 // shim layer with setTimeout fallback
     window.requestAnimFrame = (function(){
       return  window.requestAnimationFrame       || 
@@ -99,4 +150,17 @@ function createProgram(gl, vertexShader, fragmentShader) {
               };
     })();
 
-glSetup();
+if(window.attachEvent) {
+    window.attachEvent('onload', glSetup);
+} else {
+    if(window.onload) {
+        var curronload = window.onload;
+        var newonload = function() {
+            curronload();
+            glSetup();
+        };
+        window.onload = newonload;
+    } else {
+        window.onload = glSetup;
+    }
+}
