841e80aa5d5f94fd334bb7e92431e5e45770d661
John Kelly <johkelly@mymail.mines.edu>
2012-10-22 18:07:30 -0600

    WebGL boilerplate for testing.

--- rubiks.js
+++ rubiks.js
@@ -1,30 +1,102 @@
-gl=null;
+var gl;
+
 function glSetup() {
 
-	var canvas = document.getElementById("glArea");
-	console.log("Initing gl for canvas: " + canvas);
-	//Initialize the global variable gl to null.
-	gl=null;
-	try{
-		//Try to grab the standard context. If it fails,fallback to experimental.
-		gl=canvas.getContext("webgl")||canvas.getContext("experimental-webgl");
-	} catch(e) {}
+	var canvas = getElementById("drawingarea");
 
-	//If we don't have a GLcontext, give up now
-	if(!gl) {
-		alert("UnabletoinitializeWebGL.Yourbrowsermaynotsupportit.");
-		return;
-	}
-	console.log("Done initing");
+	initWebGl(canvas);
 
 	if (gl) {
 		gl.clearColor(0.0, 69.0, 0.0, 1.0);
 		gl.enable(gl.DEPTH_TEST);
 		gl.depthFunc(gl.LEQUAL);
-		gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
+		gl.clear(gl.COLOR_BUFFER_BIT|g.DEPTH_BUFFER_BIT);
 	}
 }
-function loaded() {
-	glSetup();
+
+function initWebGL(canvas) {
+	//Initializetheglobalvariablegltonull.
+	gl=null;
+	try{
+		//Trytograbthestandardcontext.Ifitfails,fallbacktoexperimental.
+		gl=canvas.getContext("webgl")||canvas.getContext("experimental-webgl");
+	}
+	catch(e){}
+
+	//If we don't have a GLcontext, give up now
+	if(!gl) {
+		alert("UnabletoinitializeWebGL.Yourbrowsermaynotsupportit.");
+	}
 }
-window.onload = function(){loaded()};
+
+/**
+ * Creates and compiles a shader.
+ *
+ * @param {!WebGLRenderingContext} gl The WebGL Context.
+ * @param {string} shaderSource The GLSL source code for the shader.
+ * @param {number} shaderType The type of shader, VERTEX_SHADER or
+ *     FRAGMENT_SHADER.
+ * @return {!WebGLShader} The shader.
+ */
+function compileShader(gl, shaderSource, shaderType) {
+  // Create the shader object
+  var shader = gl.createShader(shaderType);
+
+  // Set the shader source code.
+  gl.shaderSource(shader, shaderSource);
+
+  // Compile the shader
+  gl.compileShader(shader);
+
+  // Check if it compiled
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (!success) {
+    // Something went wrong during compilation; get the error
+    throw "could not compile shader:" + gl.getShaderInfoLog(shader);
+  }
+
+  return shader;
+}
+
+/**
+ * Creates a program from 2 shaders.
+ *
+ * @param {!WebGLRenderingContext) gl The WebGL context.
+ * @param {!WebGLShader} vertexShader A vertex shader.
+ * @param {!WebGLShader} fragmentShader A fragment shader.
+ * @return {!WebGLProgram} A program.
+ */
+function createProgram(gl, vertexShader, fragmentShader) {
+  // create a program.
+  var program = gl.createProgram();
+
+  // attach the shaders.
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+
+  // link the program.
+  gl.linkProgram(program);
+
+  // Check if it linked.
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (!success) {
+      // something went wrong with the link
+      throw ("program filed to link:" + gl.getProgramInfoLog (program));
+  }
+
+  return program;
+};
+
+// shim layer with setTimeout fallback
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     || 
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+
+glSetup();
