bf52be1acd85b09e1da009a166af1f48a8ce6d05
John Kelly <johkelly@mymail.mines.edu>
2012-10-26 11:56:31 -0600

    Proof of Concepts for: Camera motion, subset rotation, mouse event handling (down, up, move).

--- rubiks.js
+++ rubiks.js
@@ -1,3 +1,5 @@
+var mouseDown = false;
+
 function doStuff(){
 	/*
 	http://www.aerotwist.com/tutorials/getting-started-with-three-js/
@@ -6,6 +8,25 @@ function doStuff(){
 	// Grab the container canvas
 	var canvas = document.getElementById("drawingArea");
 	var $container = $('#attach');
+	
+	// Add mouse events
+	// $container.mousedown( function() {
+		// mouseDown = true;
+		// console.log("Mouse down");
+	// });
+	// $container.mouseup( function() {
+		// if(mouseDown){
+			// console.log("Valid click!");
+			// mouseDown = false;
+			// return;
+		// }
+		// console.log("Mouse up, no MD");
+	// });
+	// $container.mousemove( function(event) {
+		// if(mouseDown){
+			// console.log("Active motion at" + event.pageX + " " + event.pageY);
+		// }
+	// });
 	
 	// Set viewport size
 	//var WIDTH = canvas.getAttribute("WIDTH");
@@ -29,7 +50,10 @@ function doStuff(){
 	scene.add(camera);
 	
 	// Camera starts at origin, so move it back
-	camera.position.z = 10;
+	camera.position.z = 7;
+	camera.position.x = 7;
+	camera.position.y = 7;
+	camera.lookAt(new THREE.Vector3(0,0,0));
 	
 	// Go Go Gadget Renderer~!
 	renderer.setSize(WIDTH, HEIGHT);
@@ -37,6 +61,35 @@ function doStuff(){
 	// Attach renderer-supplied DOM element
 	// wut?
 	$container.append(renderer.domElement);
+	$canvas = renderer.domElement
+	$canvas.style.border ='1px solid black';
+	// Add canvas mouse events
+	$canvas.addEventListener( 
+	'mousedown',
+	function() {
+		mouseDown = true;
+		console.log("Mouse down");
+	},
+	false);
+	$canvas.addEventListener(
+	'mouseup',
+	function() {
+		if(mouseDown){
+			console.log("Valid click!");
+			mouseDown = false;
+			return;
+		}
+		console.log("Mouse up, no MD");
+	},
+	false);
+	$canvas.addEventListener(
+	'mousemove',
+	function(event) {
+		if(mouseDown){
+			console.log("Active motion at" + event.layerX + " " + event.layerY);
+		}
+	},
+	false);
 	
 	// Set up sphere vars
 	var height = 1,
@@ -131,6 +184,9 @@ function doStuff(){
 	// Add to scene
 	scene.add(ambient);
 	
+	// Test with subset of cubes
+	var subSet = rubiks.cubes[0];
+	
 	renderer.render(scene, camera);
 	var frame = 0;
 		(function animloop(){
@@ -138,9 +194,19 @@ function doStuff(){
 			//pointLight.position.x = 100 * Math.sin(frame/10.0);
 			//pointLight.position.y = 100 * Math.sin(frame/10.0);
 			//pointLight.position.z = 100 * Math.cos(frame/10.0);
-			rubiks.rotateX(.1 * Math.cos(frame/10.0));
-			rubiks.rotateY(0.03);
+			//rubiks.rotateX(.1 * Math.cos(frame/10.0));
+			//rubiks.rotateY(0.03);
 			//rubiks.rotateZ(0.05);
+			var rot = new THREE.Matrix4();
+			rot.rotateByAxis(new THREE.Vector3(0,1,0), 0.01);
+			camera.applyMatrix(rot);
+			rot = new THREE.Matrix4();
+			rot.rotateByAxis(new THREE.Vector3(1,0,0), 0.01);
+			for(var j=0; j < 3; ++j){
+				for(var k=0; k < 3; ++k){
+					subSet[j][k].applyMatrix(rot);
+				}
+			}
 			frame += 1;
 			renderer.render(scene, camera);
 		})();
